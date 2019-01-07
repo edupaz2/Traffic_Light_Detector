89501f15dff1ee2920baa51cb5c31e393ab11aed
Eduardo Paz <edupaz2@gmail.com>
2019-01-07 16:44:11 +0800

    feat: improving test_model.py

--- scripts/test_model.py
+++ scripts/test_model.py
@@ -4,12 +4,41 @@ import tensorflow as tf
 import cv2
 import numpy as np
 import time
+import argparse
 
+def load_and_prepare_image(image, img_size=224):
+    """
+    :param filename:
+    :param img_size:
+    :return:
+    """
+    img = cv2.resize(image, dsize=(img_size, img_size))
+    img = img.astype(np.float32)
+    img = np.expand_dims(img, 0)
+
+    # Preprocess
+    img = (img - 127.) / 127.
+
+    return img
+
+def load_image_tensor(image, input_h, input_w, input_m, input_s):
+	dims_expander = tf.expand_dims(image, 0)
+	resized = tf.image.resize_bilinear(dims_expander, [input_h, input_w])
+	normalized = tf.divide(tf.subtract(resized, [input_m]), [input_s])
+	return normalized
 
 if __name__ == '__main__':
 
 	model_file = 'models/mobilenet_1.0_224.pb'
 	labels = ['green', 'none', 'red', 'yellow']
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--image", help="image to be processed")
+	args = parser.parse_args()
+
+	file_name = 'test_pics/001.png'
+	if args.image:
+		file_name = args.image
 
 	graph = tf.Graph()
 	graph_def = tf.GraphDef()
@@ -24,38 +53,14 @@ if __name__ == '__main__':
 
 	tt = t = time.time()
 	with tf.Session(graph=graph) as sess:
-		newt = time.time()
-		print("Time1: {0}".format(newt-t))
-		t=newt
 		input_h = 224
 		input_w = 224
 		input_m = 0
 		input_s = 255
 
-		image = cv2.imread('test_pics/001.png')
-		newt = time.time()
-		print("Time2: {0}".format(newt-t))
-		t=newt
-		#image = cv2.resize(image, dsize=(input_h, input_w), interpolation=cv2.INTER_CUBIC)
-		np_image = np.asarray(image)
-		dims_expander = tf.expand_dims(np_image, 0)
-		resized = tf.image.resize_bilinear(dims_expander, [input_h, input_w])
-		normalized = tf.divide(tf.subtract(resized, [input_m]), [input_s])
-		
-		newt = time.time()
-		print("Time3: {0}".format(newt-t))
-		t=newt
-		
-		tensor = sess.run(normalized)
-		newt = time.time()
-		print("Time4: {0}".format(newt-t))
-		t=newt
-		
+		img = cv2.imread(file_name)
+		tensor = sess.run(load_image_tensor(img, input_h, input_w, input_m, input_s))
 		results = sess.run(output_operation.outputs[0], {input_operation.outputs[0]: tensor})
-		newt = time.time()
-		print("Time5: {0}".format(newt-t))
-		t=newt
-		
 		results = np.squeeze(results)
 		top_k = results.argsort()[-5:][::-1]
-		print("Total: {0}. Result: {1}".format(time.time() - tt, ['{0}-{1}'.format(labels[i], results[i]) for i in top_k]))
+	print('Total Total: {0}'.format(time.time()-tt))
